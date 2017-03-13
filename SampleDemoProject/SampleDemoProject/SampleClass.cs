5ca85d79a1632c08c4b0a5fc5b4530e6ff61da45
ranjithsundaram470 <ranjith.meenatchi@pointelsolutions.com>
2017-03-13 17:28:50 +0530

    just added one class

---
+++ SampleDemoProject/SampleDemoProject/SampleClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleDemoProject
+{
+    class SampleClass
+    {
+    }
+}
